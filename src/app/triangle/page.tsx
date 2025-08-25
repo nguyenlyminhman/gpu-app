0837626b1234cb283ad825cc157ac24398d21edb
man nguyen <minhman2.nguyen@mafc.com.vn>
2025-08-25 17:17:42 +0700

    triangle demo

--- src/app/triangle/page.tsx
+++ src/app/triangle/page.tsx
@@ -31,46 +31,20 @@ export default function Home() {
             // === Shaders (WGSL) ===
             const shaderModule = device.createShaderModule({
                 code: `
-                    struct VSOutput {
-                        @builtin(position) Position : vec4f,
-                        @location(0) color : vec4f
-                    };
-
                     @vertex
-                    fn vs_main(@builtin(vertex_index) VertexIndex : u32) -> VSOutput {
-                        var pos = array<vec2f, 6>(
-                            // Tam giác 1
-                            vec2f( 0.0,  0.5),   
-                            vec2f(-0.5, -0.5),   
-                            vec2f( 0.5, -0.5),   
-
-                            // Tam giác 2
-                            vec2f( 0.0, -0.2),   
-                            vec2f(-0.5, -0.8),   
-                            vec2f( 0.5, -0.8)    
+                    fn vs_main(@builtin(vertex_index) VertexIndex : u32)  -> @builtin(position) vec4f {
+                        var pos = array<vec2f, 3>(
+                            vec2f( 0.0,  0.5),   // top
+                            vec2f(-0.5, -0.5),   // bottom left
+                            vec2f( 0.5, -0.5),   // bottom right
                         );
 
-                        var colors = array<vec4f, 6>(
-                            // Tam giác 1 màu đỏ
-                            vec4f(1.0, 0.0, 0.0, 1.0),
-                            vec4f(1.0, 0.0, 0.0, 1.0),
-                            vec4f(1.0, 0.0, 0.0, 1.0),
-
-                            // Tam giác 2 màu xanh
-                            vec4f(0.0, 0.0, 1.0, 1.0),
-                            vec4f(0.0, 0.0, 1.0, 1.0),
-                            vec4f(0.0, 0.0, 1.0, 1.0)
-                        );
-
-                        var output: VSOutput;
-                        output.Position = vec4f(pos[VertexIndex], 0.0, 1.0);
-                        output.color = colors[VertexIndex];
-                        return output;
+                        return vec4f(pos[VertexIndex], 0.0, 1.0);
                     }
 
                     @fragment
-                    fn fs_main(in: VSOutput) -> @location(0) vec4f {
-                        return in.color;
+                    fn fs_main() -> @location(0) vec4f {
+                        return vec4f(1.0, 0.0, 0.0, 1.0); // red color
                     }
                     `
             });
